3a93bea1f4983b1cd85a6e00af4af8da5ae53f94
tomtam6@gmail.com <tomtam6@gmail.com>
2018-07-20 14:36:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(obj, key){
+<<<<<<< HEAD
   // assign original object to a clone Object
   var newObj = Object.assign({}, obj);
   // utilize bracket notation to access properties via variables
@@ -27,13 +28,22 @@ function deleteFromObjectByKey(obj, key){
   // so if dot notation is used, then it takes the 'key' paramater as a literal 
   // to find the property of 'key' and not what 'key' is supposed to represent
   delete newObj[key];
+=======
+  var newObj = Object.assign({}, obj);
+  delete newObj.key;
+>>>>>>> 8f05aef2bb84a64157192729951e7b20694cdd0e
   return newObj;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
+<<<<<<< HEAD
   // same deal here, need to utilize bracket notation to access object's key
   // where 'key' is represented as a variable
   // otherwise, dot notation will take the 'key' parameter as a literal property
   delete object[key];
   return object;
+=======
+  delete object.key;
+  object;
+>>>>>>> 8f05aef2bb84a64157192729951e7b20694cdd0e
 }
